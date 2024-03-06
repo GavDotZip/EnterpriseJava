f5e65566c68043b84502c6e060d5c705afa05bd3
GavDotZip <gavdotzip@gmail.com>
2024-03-06 21:34:58 +0000

    comment commit

--- src/A1/TestCentre.java
+++ src/A1/TestCentre.java
@@ -1,4 +1,25 @@
 package A1;
 
+// Class representing a test centre
 public class TestCentre {
+
+    // Name and address of the test centre
+    private String name;
+    private String address;
+
+    // Constructor to initialize the test centre with name and address
+    public TestCentre(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    // Getter method to retrieve the name of the test centre
+    public String getName() {
+        return name;
+    }
+
+    // Getter method to retrieve the address of the test centre
+    public String getAddress() {
+        return address;
+    }
 }
