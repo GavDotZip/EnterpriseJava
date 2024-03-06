86a9ad3b0121bb01e8df01db78465c89d7903461
GavDotZip <gavdotzip@gmail.com>
2024-03-06 21:44:45 +0000

    commit

--- src/A1/BookNCTTest.java
+++ src/A1/BookNCTTest.java
@@ -19,7 +19,7 @@ public class BookNCTTest{
     public void testRegNO_NoEditRegNO() throws InvalidDateTimeException {
         String regNumQueryInput = "16-WH-59741";
         TestCentre centre1 = new TestCentre("Ballinasloe", "Unit 9, Pollboy Industrial Estate, Ballinasloe, Galway H53 NW94");
-        NCTBooking booking1 = new BookNCT(regNumQueryInput, centre1, LocalDateTime.of(2021, Month.OCTOBER, 22, 8, 0));
+        BookNCT booking1 = new BookNCT(regNumQueryInput, centre1, LocalDateTime.of(2021, Month.OCTOBER, 22, 8, 0));
         String regNumQueryResult = booking1.queryRegNum();
         assertEquals(regNumQueryResult, regNumQueryInput, "Registration number query successful");
     }
